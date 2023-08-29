687aa2805e3ca4eefdcb0d727ece1c88cc45f8a9
A. Urquiola <patchamama.go@gmail.com>
2023-08-30 01:50:32 +0200

    Add DB access and operations

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 /* eslint-disable comma-dangle */
 /* eslint-disable semi */
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const Note = require('./models/note');
 
 const requestLogger = (request, response, next) => {
   console.log(
@@ -16,15 +18,14 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
-};
-
 // midlewares
-app.use(cors());
-app.use(express.json());
-app.use(requestLogger);
+// Serving static files from the backend
 app.use(express.static('dist'));
+// To parse the body of the request
+app.use(express.json());
+// Same origin policy
+app.use(cors());
+app.use(requestLogger);
 
 let notes = [
   {
@@ -48,59 +49,132 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>');
 });
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes);
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
+// app.get('/api/notes', (req, res) => {
+//   res.json(notes);
+// });
+
+// const generateId = () => {
+//   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+//   return maxId + 1;
+// };
 
 app.post('/api/notes', (request, response) => {
   const body = request.body;
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing',
-    });
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
+
+// app.post('/api/notes', (request, response) => {
+//   const body = request.body;
+
+//   if (!body.content) {
+//     return response.status(400).json({
+//       error: 'content missing',
+//     });
+//   }
+
+//   const note = {
+//     content: body.content,
+//     important: body.important || false,
+//     date: new Date(),
+//     id: generateId(),
+//   };
+
+//   notes = notes.concat(note);
+
+//   response.json(note);
+// });
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
-  }
-
-  response.json(note);
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
+// app.get('/api/notes/:id', (request, response) => {
+//   const id = Number(request.params.id);
+//   const note = notes.find((note) => note.id === id);
 
-  response.status(204).end();
+//   if (note) {
+//     response.json(note);
+//   } else {
+//     response.status(404).end();
+//   }
+
+//   response.json(note);
+// });
+
+// app.delete('/api/notes/:id', (request, response) => {
+//   const id = Number(request.params.id);
+//   notes = notes.filter((note) => note.id !== id);
+
+//   response.status(204).end();
+// });
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
+
+// Invalid routes
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
 app.use(unknownEndpoint);
+// Error handling in middleware
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
 
-const PORT = process.env.PORT || 3001;
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+  next(error);
+};
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
---
+++ mongo.js
@@ -0,0 +1,46 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable semi */
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+  console.log(
+    'Please provide the password as an argument: node mongo.js <password>'
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://mandy_phonebook:${password}@cluster0.za4sftc.mongodb.net/noteApp?retryWrites=true&w=majority`;
+
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+});
+
+const Note = mongoose.model('Note', noteSchema);
+
+// const note = new Note({
+//   content: 'RGET and POST are the most important methods of HTTP protocol',
+//   date: new Date(),
+//   important: false,
+// });
+
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
+
+// note.save().then((result) => {
+//   console.log('note saved!');
+//   mongoose.connection.close();
+// });
+
+// mandy_phonebook
+// T0pb3QiCPxUCeFdd
+// mongodb+srv://mandy_phonebook:T0pb3QiCPxUCeFdd@cluster0.za4sftc.mongodb.net/phonebook?retryWrites=true&w=majority
