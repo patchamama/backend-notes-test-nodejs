dde06384f95014139b8ebf3b23078c9b823f9320
A. Urquiola <patchamama.go@gmail.com>
2023-09-08 13:57:04 +0200

    Restructuring in folders the code to better practices

--- models/note.js
+++ models/note.js
@@ -1,46 +1,24 @@
-/* eslint-disable comma-dangle */
-/* eslint-disable semi */
-require('dotenv').config();
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
-
-// eslint-disable-next-line no-undef
-const url = process.env.MONGODB_URI;
-
-console.log('connecting to', url);
-
-mongoose
-  .connect(url)
-  .then(() => {
-    console.log('connected to MongoDB');
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message);
-  });
+const mongoose = require('mongoose')
 
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minlength: 5,
     required: true,
-    unique: true,
+    minlength: 5,
   },
   date: {
     type: Date,
     required: true,
   },
   important: Boolean,
-});
-
-// Apply the uniqueValidator plugin to noteSchema.
-noteSchema.plugin(uniqueValidator);
+})
 
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model('Note', noteSchema);
+module.exports = mongoose.model('Note', noteSchema)
