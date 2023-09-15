b45b46d370d62ac2a08cbe936262c399edcd1bb1
A. Urquiola <patchamama.go@gmail.com>
2023-09-15 12:36:54 +0200

    add users model

--- models/note.js
+++ models/note.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose')
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    required: true,
     minlength: 5,
-  },
-  date: {
-    type: Date,
     required: true,
   },
   important: Boolean,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
 })
 
 noteSchema.set('toJSON', {
